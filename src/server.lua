fb7a29101ec0ee08ece3793187e64e620bd10278
ra100 <ra100x@gmail.com>
2016-04-30 12:38:30 +0200

    swithc to pushing metrics

--- src/server.lua
+++ src/server.lua
@@ -6,37 +6,26 @@ function get_metrics()
   local d = "# HELP power_meter Power consumption in Wh\n"
   .. "# TYPE power_meter counter\n"
   .. "power_meter{graphname=\"power_meter\",label=\"power\",type=\"counter\"} " .. tostring(c) .. "\n"
+  .. "# HELP nodemcu_heap nodeMCU heap\n"
+  .. "# TYPE nodemcu_heap gauge\n"
+  .. "nodemcu_heap{graphname=\"nodemcu\",label=\"heap\",type=\"gauge\"} " .. tostring(node.heap()) .. "\n"
+  .. "# HELP nodemcu_vdd33 nodeMCU system voltage\n"
+  .. "# TYPE nodemcu_vdd33 gauge\n"
+  .. "nodemcu_vdd33{graphname=\"nodemcu\",label=\"vdd33\",type=\"gauge\"} " .. tostring(adc.readvdd33()/1000) .. "\n";
   return d
 end
 
-function init_server()
-  -- create a server
-  -- 30s time out for a inactive client
-  local sv = net.createServer(net.TCP, 30)
-
-  -- server listen on 80
-  sv:listen(80,function(conn)
-    conn:on("receive", function(client, pl)
-      if string.match(pl, "metrics") then
-        -- print(pl)
-        local buff = ""
-        buff = buff .. "HTTP/1.1 200 OK\n"
-        buff = buff .. "Content-Type:text/plain; version=0.0.4\n"
-        buff = buff .. "\n"
-        buff = buff .. get_metrics()
-        buff = buff .. "# HELP nodemcu_heap nodeMCU heap\n"
-        .. "# TYPE nodemcu_heap gauge\n"
-        .. "nodemcu_elog_heap{graphname=\"nodemcu\",label=\"heap\",type=\"gauge\"} " .. tostring(node.heap()) .. "\n";
-        buff = buff .. "# HELP nodemcu_vdd33 nodeMCU system voltage\n"
-        .. "# TYPE nodemcu_vdd33 gauge\n"
-        .. "nodemcu_elog_vdd33{graphname=\"nodemcu\",label=\"vdd33\",type=\"gauge\"} " .. tostring(adc.readvdd33()/1000) .. "\n";
-        client:send(buff)
-      else
-        client:send("OK")
-      end
-      client:close()
+function push_metrics()
+  http.post(PUSHGATEWAY,
+  'Content-Type: text/plain; version=0.0.4\r\n',
+  get_metrics(),
+  function(code, data)
+    if (code < 0) then
+      print("HTTP request failed")
+      tmr.alarm(2, 60*1000, 0, push_metrics)
+    else
       collectgarbage()
-     end)
+    end
   end)
 end
 
@@ -45,12 +34,13 @@ elog.init(PIN)
 wifi.sta.eventMonReg(wifi.STA_GOTIP, function()
   wifi.sta.eventMonStop("unreg all")
   print(wifi.sta.getip())
-  init_server()
+  push_metrics()
+  tmr.alarm(0, PUSHINTERVAL * 1000, 1, push_metrics)
 end)
 
 wifi.sta.eventMonStart()
 
-tmr.alarm(1, RESTART_INTERVAL*1000, 1, function()
+tmr.alarm(1, RESTARTINTERVAL*1000, 1, function()
   local c = elog.getCounter()
   if c == counter then
     node.restart()
